9dc180fb7864b86c5b40c4783a378a27907ef498
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-02-21 21:27:35 -0600

    showpost working

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function ShowPost(props) {
    const { postId } = useParams()
+  
    //const data = props.data[postId]
 
    let [data, setData] = useState({})
 
    useEffect(() => {
       const fetchData = async () => {
-         const response = await fetch(`http://localhost:3000/posts`)
+         const response = await fetch(`http://localhost:3000/posts/${postId}`)
          const resData = await response.json()
-         setData(resData[postId])
-         console.log(resData)
+         setData(resData)
       }
       fetchData()
    }, [])
