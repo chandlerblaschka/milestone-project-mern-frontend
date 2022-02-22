1a574e0993f1bba0d18ae9b5d4afa0e10786d6b0
unknown <derek.slauson5@gmail.com>
2022-02-21 18:55:39 -0600

    Fetches all posts

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import BlogNavBar from './components/BlogNavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Gallery from './components/Gallery';
 import NewPost from './components/NewPost';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  let [data, setData] = useState({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:3000/posts`)
+      const resData = await response.json()
+      setData(resData)
+      console.log(resData)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <Router>
         <BlogNavBar />
         <Routes>
           <Route path="/" element={
-            <Gallery />
+            <Gallery data={data} />
           } />
           {/* NEW POST */}
           <Route path="/newPost" element={
