772af3e91ff999fa21437807dbea03dc74496e25
unknown <derek.slauson5@gmail.com>
2022-02-21 19:46:46 -0600

    New post form submits

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form"
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Button from "react-bootstrap/Button";
@@ -37,9 +37,34 @@ const NewPost = () => {
         textAlign: "center"
     }
 
+    let [author, setAuthor] = useState("")
+    let [title, setTitle] = useState("")
+    let [date, setDate] = useState("")
+    let [content, setContent] = useState("")
+
+    const submitPost = async (e) => {
+        e.preventDefault()
+        const post = {
+            post_author: author,
+            post_title: title,
+            post_date: date,
+            post_content: content
+        }
+
+        await fetch("http://localhost:3000/posts", {
+            method: "POST",
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(post)
+        })
+        window.location = "/"
+    }
+
     return (
         <div>
-            <Form style={formStyle}>
+            <Form style={formStyle} onSubmit={(e) => submitPost(e)}>
                 <Form.Group style={{ display: "flex", flexDirection: "column" }}>
                     <div style={nameDateStyle}>
                         <FloatingLabel
@@ -48,23 +73,23 @@ const NewPost = () => {
                             className="mb-3"
                             style={formSpacingLeft}
                         >
-                            <Form.Control type="text" placeholder="John Doe" />
+                            <Form.Control type="text" placeholder="John Doe" onChange={(e) => setAuthor(e.target.value)} />
                         </FloatingLabel>
-                        <Form.Control className="mb-3" size="md" type="date" style={formSpacingRight} />
+                        <Form.Control className="mb-3" size="md" type="date" style={formSpacingRight} onChange={(e) => setDate(e.target.value)} />
                     </div>
                     <FloatingLabel
                         controlId="floatingInput"
                         label="Blog Title"
                         className="mb-3"
                     >
-                        <Form.Control type="text" placeholder="My Blog Post" />
+                        <Form.Control type="text" placeholder="My Blog Post" onChange={(e) => setTitle(e.target.value)} />
                     </FloatingLabel>
                     <FloatingLabel
                         controlId="floatingInput"
                         label="Blog Content"
                         className="mb-3"
                     >
-                        <Form.Control as="textarea" placeholder="Create a blog here!" style={textAreaStyle} />
+                        <Form.Control as="textarea" placeholder="Create a blog here!" style={textAreaStyle} onChange={(e) => setContent(e.target.value)} />
                     </FloatingLabel>
                 </Form.Group>
                 <div style={buttonStyle}>
