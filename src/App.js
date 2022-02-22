62adec3b67b45be87e1dff6e59dcb821349c20d1
chandlerblaschka <cblaschka@live.com>
2022-02-21 18:06:24 -0600

    card spacing and new post form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import BlogNavBar from './components/BlogNavBar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div className="App">
-      <Gallery />
+      <Router>
+        <BlogNavBar />
+        <Routes>
+          <Route path="/" element={
+            <Gallery />
+          } />
+          {/* NEW POST */}
+          {/* <Route path="/newPost" element={
+            
+          } /> */}
+          {/* EDIT POST */}
+          {/* <Route path="/editPost" element={
+
+          } /> */}
+          {/* POST SHOW PAGE */}
+          {/* <Route path="/postShow/:postId" element={
+
+          } /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
