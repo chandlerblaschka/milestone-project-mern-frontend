9fe57626d16246dd8483da1a701ce2d287fe267a
Nik Poliarco <nikpoliarco@Niks-MacBook-Pro.local>
2022-02-23 18:50:03 -0600

    added css

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,13 +1,30 @@
+import dateFormat from "dateformat"
 import React, { useEffect, useState } from "react"
-import { Button } from "react-bootstrap"
-import { useNavigate, useParams } from "react-router-dom"
-
+import { Button, FloatingLabel, Form } from "react-bootstrap"
+import { Link, useParams } from "react-router-dom"
+import "./showpost.css"
 export default function ShowPost(props) {
   const { postId } = useParams()
-  //const data = props.data[postId]
-  const navigate = useNavigate()
+  let [author, setAuthor] = useState("")
   let [data, setData] = useState({})
+  let [date, setDate] = useState(Date.now())
 
+  const dateFormatted = dateFormat(date, "yyyy-mm-dd")
+  const formSpacingRight = {
+    paddingLeft: "16px",
+    display: "flex",
+    width: "50%",
+  }
+
+  const formSpacingLeft = {
+    paddingRight: "16px",
+    display: "flex",
+    width: "50%",
+  }
+  const nameDateStyle = {
+    display: "flex",
+    flexDirection: "row",
+  }
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(`http://localhost:3000/posts/${postId}`)
@@ -18,10 +35,44 @@ export default function ShowPost(props) {
   }, [])
 
   return (
-    <div>
-      <h1>{data.post_title}</h1>
+    <div className='showPost'>
+      <center>
+        <h1 className='postTitle'>{data.post_title}</h1>
+      </center>
+      <h1>{data.post_author}</h1>
+      <h1 className='postDate'>{data.post_date}</h1>
+      <p className='postDesc'>{data.post_content}</p>
       <Button onClick={() => props.deletePost(postId)}>DELETE</Button>{" "}
-      <Button href='#'>EDIT</Button>
+      <Link to={`/editPost/${postId}`}>
+        {/* <Button href='#'>EDIT</Button> */}Edit
+      </Link>
+      <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+        <div style={nameDateStyle}>
+          <FloatingLabel
+            controlId='floatingInput'
+            label='Your Name'
+            className='mb-3'
+            style={formSpacingLeft}
+          >
+            <Form.Control
+              type='text'
+              placeholder='John Doe'
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </FloatingLabel>
+          <Form.Control
+            className='mb-3'
+            size='md'
+            type='date'
+            style={formSpacingRight}
+            defaultValue={dateFormatted}
+            disabled
+          />
+        </div>
+        <Form.Label>Comment</Form.Label>
+        <Form.Control as='textarea' rows={3} />
+      </Form.Group>
+      <Button>Submit</Button>
     </div>
   )
 }
