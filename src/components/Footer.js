cd8de24b9e8e92027c6c3ea71aaf44907a7e9ad4
unknown <derek.slauson5@gmail.com>
2022-02-25 19:15:28 -0600

    Finished simple footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
-import { Navbar, Container, Nav } from "react-bootstrap"
+import { Navbar, Container } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
         <Navbar expand="lg" variant="light" bg="light">
             <Container>
-                <Navbar.Brand href="#">Blog.it</Navbar.Brand>
+                <Link to="/" className="nav-link"  >
+                    <Navbar.Brand style={{ color: "#ECA72C", border: "#ECA72C 2px solid", padding: "5px 10px 5px 10px" }}>Blog.it</Navbar.Brand>
+                </Link>
                 <Container>
-                    {/* align right? */}
-                    Made by <a href="https://www.linkedin.com/in/nik-poliarco-3aaa181a3/" target="_blank">Albert Nikolai Poliarco</a>, <a href="https://www.linkedin.com/in/chandler-blaschka-532547b2/" target="_blank">Chandler Blaschka</a>, and <a href="#">Derek Slauson</a>
+                    Made by <a href="https://www.linkedin.com/in/nik-poliarco-3aaa181a3/" target="_blank" rel="noreferrer">Albert Nikolai Poliarco</a>, <a href="https://www.linkedin.com/in/chandler-blaschka-532547b2/" target="_blank" rel="noreferrer">Chandler Blaschka</a>, and <a href="https://portfolio-dfs.herokuapp.com/" target="_blank" rel="noreferrer">Derek Slauson</a>
                 </Container>
             </Container>
         </Navbar>
